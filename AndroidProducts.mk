3971ccfe465273b4956d648cc0df33008444ed21
REV3NT3CH <rev3nt3ch@gmail.com>
2020-03-22 03:28:06 -0700

    Bring in GZOSP peripherals

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -18,5 +18,5 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/aosp_taimen.mk \
     $(LOCAL_DIR)/gzosp_taimen.mk
 
-COMMON_LUNCH_CHOICES := \
-    aosp_taimen-userdebug
+#COMMON_LUNCH_CHOICES := \
+#    aosp_taimen-userdebug
--- gzosp_taimen.mk
+++ gzosp_taimen.mk
@@ -2,24 +2,24 @@
 TARGET_SCREEN_HEIGHT := 2880
 TARGET_SCREEN_WIDTH := 1440
 
-# Inherit some common Lineage stuff.
+# Inherit some common GZOSP stuff.
 $(call inherit-product, vendor/gzosp/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/google/taimen/aosp_taimen.mk)
 
--include device/google/taimen/device-lineage.mk
-
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := gzosp_taimen
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Pixel 2 XL
+TARGET_MANUFACTURER := LG
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=taimen \
-    PRIVATE_BUILD_DESC="taimen-user 10 QQ2A.200405.005 6254899 release-keys"
+    PRIVATE_BUILD_DESC="taimen-user 9 PPR1.180610.009 4898911 release-keys"
 
-BUILD_FINGERPRINT := google/taimen/taimen:10/QQ2A.200405.005/6254899:user/release-keys
+BUILD_FINGERPRINT := google/taimen/taimen:9/PPR1.180610.009/4898911:user/release-keys
 
 $(call inherit-product-if-exists, vendor/google/taimen/taimen-vendor.mk)
+$(call inherit-product-if-exists, vendor/gapps/gapps.mk)
