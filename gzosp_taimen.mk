8316d55bf429cd098d664d86c030c6e9567fd472
roguesyko <roguesyko@gmail.com>
2020-05-05 17:17:52 -0400

    gzosp

--- gzosp_taimen.mk
+++ gzosp_taimen.mk
@@ -3,7 +3,7 @@ TARGET_SCREEN_HEIGHT := 2880
 TARGET_SCREEN_WIDTH := 1440
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/gzosp/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/google/taimen/aosp_taimen.mk)
@@ -11,7 +11,7 @@ $(call inherit-product, device/google/taimen/aosp_taimen.mk)
 -include device/google/taimen/device-lineage.mk
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := lineage_taimen
+PRODUCT_NAME := gzosp_taimen
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Pixel 2 XL
 PRODUCT_RESTRICT_VENDOR_FILES := false
